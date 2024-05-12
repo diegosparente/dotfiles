3aae5327e603c87b5baebfed02f5f44c4e3366bd
Diego Parente <diego.s.parente@gmail.com>
2024-05-12 18:34:45 -0300

    Update android.fish

--- conf.d/android.fish
+++ conf.d/android.fish
@@ -1,8 +1,12 @@
 #REACT-NATIVE CONFIG | Android SDK
-set --export ANDROID_HOME $HOME/.android/Sdk;
+set --export ANDROID_HOME $HOME/.Android/Sdk;
 
-set -gx PATH $ANDROID_HOME/cmdline-tools/latest $PATH;
-set -gx PATH $ANDROID_HOME/cmdline-tools/latest/bin $PATH;
+#set -gx PATH $ANDROID_HOME/cmdline-tools/latest $PATH;
+#set -gx PATH $ANDROID_HOME/cmdline-tools/latest/bin $PATH;
+set -gx PATH $ANDROID_HOME/cmdline-tools/10.0 $PATH;
+set -gx PATH $ANDROID_HOME/cmdline-tools/10.0/bin $PATH;
+#set -gx PATH $ANDROID_HOME/tools $PATH;
+#set -gx PATH $ANDROID_HOME/tools/bin $PATH;
 set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+#set -gx PATH $ANDROID_HOME/build-tools $PATH;
 set -gx PATH $ANDROID_HOME/emulator $PATH;
-set -gx PATH $ANDROID_HOME/Sdk $PATH;
