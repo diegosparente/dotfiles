0619d01649e7dbd3936fda03fb81c82ee78e33f9
Diego Parente <diego.s.parente@gmail.com>
2025-04-22 15:15:18 -0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -8,11 +8,9 @@ starship init fish | source
 
 $HOME/.local/bin/mise activate fish | source
 
-alias kubectl="minikube kubectl --"
-
 if status is-interactive
     set -g fish_history_size 10000
-    set -g fish_history_merge_mode overwrite
-
-    set -g fish_features stderr-nocaret qmark-noglob ampersand-nobg-in-token
+	#set -g fish_history_merge_mode overwrite
+	#set -g fish_features stderr-nocaret qmark-noglob ampersand-nobg-in-token
+    echo "🐟 Fish ready for dev with Mise, Minikube, and Starship!"
 end
