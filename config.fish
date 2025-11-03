e9639e5018c9af066aa0744f378a120cddbdc48e
Diego Parente <diego.s.parente@gmail.com>
2025-11-03 18:33:09 -0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,16 +1,13 @@
 for config_file in ~/.config/fish/conf.d/*.fish
-    if test -f $config_file
-        source $config_file
-    end
+    test -f $config_file; and source $config_file
 end
 
-starship init fish | source
-
 $HOME/.local/bin/mise activate fish | source
+starship init fish | source
 
 if status is-interactive
     set -g fish_history_size 10000
-	#set -g fish_history_merge_mode overwrite
-	#set -g fish_features stderr-nocaret qmark-noglob ampersand-nobg-in-token
+    # set -g fish_history_merge_mode overwrite
+    # set -g fish_features stderr-nocaret qmark-noglob ampersand-nobg-in-token
     echo "🐟 Fish ready for dev with Mise, Minikube, and Starship!"
 end
